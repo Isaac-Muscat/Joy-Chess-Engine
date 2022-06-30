447e484ed1ec27b4dbc33e22eb0a41d9fef72539
Isaac Muscat <isaac.wieting.muscat@gmail.com>
2022-06-30 12:21:01 -0400

    Deleted replit files

--- .ccls-cache/@home@runner@Chess-Engine/main.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "ChessEngine.hpp"
-
-int main() {
-    JoyChess::InitBitboards();
-    for (int i = 0; i < JoyChess::NUM_SQUARES; i++)
-        std::cout << JoyChess::BitboardToString(JoyChess::ANTIDIAGONAL[i]);
-    std::cout << "Program finished!\n";
-}
